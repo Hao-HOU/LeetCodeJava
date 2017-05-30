c992c2e92f0153992dacdafde4378199b3b8d746
Hao HOU <houhao118@163.com>
2017-05-30 16:48:01 +0800

    题目3，用比较暴力的方法解决的，考虑重写

--- medium/src/main/java/com/acehouhao/P03LongestSubstringWithoutRC.java
+++ medium/src/main/java/com/acehouhao/P03LongestSubstringWithoutRC.java
@@ -1,18 +1,38 @@
 package com.acehouhao;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by Hao HOU on 2017/5/23.
  */
 public class P03LongestSubstringWithoutRC {
     public static int lengthOfLongestSubstring(String s) {
-        int length = 0;
+        int length;
+        int result = 0;
+        char[] chars = s.toCharArray();
+        Set cs = new HashSet();
+        for (int i = 0; i < chars.length; i++) {
+            length = 0;
+            for (int j = i; j < chars.length; j++) {
+                if (cs.contains(chars[j])) {
+                    break;
+                } else {
+                    cs.add(chars[j]);
+                    length++;
+                }
+            }
+            cs.clear();
+            if (length > result) {
+                result = length;
+            }
+        }
 
-
-        return length;
+        return result;
     }
 
     public static void main(String[] args) {
-        String s = "asdfdad";
+        String s = "result";
         System.out.println(lengthOfLongestSubstring(s));
     }
 }
